798b48fa8659e3cd4192638b99242b8df322cac9
Tamxaun <deus.main@gmail.com>
2022-09-08 14:06:29 +0300

    1 file modified

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,6 +1,6 @@
-import { HoudiniClient } from '$houdini';
+import { HoudiniClient, type RequestHandlerArgs } from '$houdini';
 
-async function fetchQuery({ fetch, text = '', variables = {}, session, metadata }) {
+async function fetchQuery({ fetch, text = '', variables = {}, session, metadata }:RequestHandlerArgs) {
 	const url = 'https://api.spacex.land/graphql';
 	const result = await fetch(url, {
 		method: 'POST',
