59dd1b6fa9121c5acdd9fe0527b214ef81020599
Dmitry <deus.main@gmail.com>
2022-09-11 14:36:29 +0300

    style: cleanup

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -6,8 +6,7 @@ export const load: PageLoad = async (event) => {
 
    const result = await SpacexHistories.fetch({ event, variables: { limit: 1 } });
 
-   console.log('do something with', result)
-   console.log('do something with', result.data?.histories)
+   console.log('do something with', result);
 
    return { SpacexHistories }
 }
