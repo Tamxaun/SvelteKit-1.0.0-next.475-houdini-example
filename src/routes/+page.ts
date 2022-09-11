03ea0aa3e63ad00feecd06ebb5fbc2dce8f5b5a2
Dmitry <deus.main@gmail.com>
2022-09-11 11:03:30 +0000

    Update +page.svelte and +page.ts

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -7,6 +7,7 @@ export const load: PageLoad = async (event) => {
    const result = await SpacexHistories.fetch({ event, variables: { limit: 1 } })
 
    console.log('do something with', result)
+   console.log('do something with', result.data.histories)
 
    return { SpacexHistories }
 }
